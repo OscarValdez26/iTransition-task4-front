c332fa3196eac2e2ca5825dabc7ce3f343b56ef7
OscarValdez26 <oscarrvaldez25@gmail.com>
2024-09-19 11:07:22 -0600

    revisadas URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = import.meta.env.VITE_BACKEND_URL || "https://task4-backend-1e6535aaaba6.herokuapp.com/";
+const URL = "https://task4-backend-1e6535aaaba6.herokuapp.com/";
 
 const instance = axios.create({
     baseURL:URL,
