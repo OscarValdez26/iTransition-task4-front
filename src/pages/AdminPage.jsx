ff35553963d016bc6dc624ab633200cc23c8775b
OscarValdez26 <oscarrvaldez25@gmail.com>
2024-09-19 17:13:10 -0600

    update

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -49,7 +49,7 @@ function AdminPage() {
             updateuser(element);
         });
         //getusers();
-        window.location.reload();
+        //window.location.reload();
     }
     const unlockUser = ()=>{
         let newModify = modify;
@@ -58,7 +58,7 @@ function AdminPage() {
             updateuser(element);
         });
         //getusers();
-        window.location.reload();
+        //window.location.reload();
     }
     const deleteUser = ()=>{
         let newModify = modify;
@@ -67,7 +67,7 @@ function AdminPage() {
             updateuser(element);
         });
         //getusers();
-        window.location.reload();
+        //window.location.reload();
     }
     return (
         <div>
